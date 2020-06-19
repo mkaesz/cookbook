e69984b58a16cdf3564dbdbeacb211b48c30be6a
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2020-06-19 19:52:27 +0200

    nomad cluster with consul and haproxy loadbalancer as deployment

---
+++ terraform/libvirt/consul-nomad-sec-ent-lb/bastion/scripts/acl-bootstrap-nomad.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [[ ! -d "$NOMAD_TOKEN" ]]; then
+  token=$(nomad acl bootstrap | grep Secret | awk '{print $4}')
+  export NOMAD_TOKEN=$token
+  echo "export NOMAD_TOKEN=$token" >> ~/.bashrc
+fi
