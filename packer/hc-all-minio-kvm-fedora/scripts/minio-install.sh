3d3018b9754f9b95ac8a02ef973a04571224fac0
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2020-06-23 20:45:59 +0200

    added minio to kickstart

---
+++ packer/hc-all-minio-kvm-fedora/scripts/minio-install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+cd /tmp
+wget https://dl.min.io/server/minio/release/linux-amd64/minio
+chmod +x minio
+mv minio /usr/local/bin
+
+wget https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x mc
+mv mc /usr/local/bin
+
+mkdir -p /opt/minio/{config,data}
+chown -R hcops:hcops /opt/minio
+
+cat <<-EOF | sudo tee /etc/systemd/system/minio.service
+[Unit]
+Description=MinIO
+Documentation=https://docs.min.io
+Wants=network-online.target
+After=network-online.target
+AssertFileIsExecutable=/usr/local/bin/minio
+
+[Service]
+WorkingDirectory=/opt/minio
+User=hcops
+Group=hcops
+
+EnvironmentFile=/opt/minio/config/minio-server.config
+ExecStart=/usr/local/bin/minio server \$MINIO_OPTS \$MINIO_VOLUMES
+Restart=always
+LimitNOFILE=65536
+TimeoutStopSec=infinity
+SendSIGKILL=no
+
+[Install]
+WantedBy=multi-user.target
+EOF
